815d0aa694075a45e960f6c03906e4f9d854e8c8
Pedro <Pedro@DESKTOP-TRRQRFR>
2022-08-16 14:09:45 +0200

    comin

---
+++ ExamenUF2406-2/test/controlador/FuncionesTest.java
@@ -0,0 +1,45 @@
+package controlador;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FuncionesTest {
+
+	@Test
+	void rotarDerechaTest() {
+		int v[]= {1,5,9};
+		int central[] = {5};
+		calc.rotarDerecha(v);
+		assertArrayEquals("El numero central es {5}",
+				central,v);
+	}
+		
+	
+	@Test
+	void esbisiesto() {
+		assertTrue(calc.esbisiesto(2020),
+			"El numero 2020 deberia devolver true");
+		assertFalse(calc.esbisiesto(2022),
+			"El numero 2022 deberia devolver false");	
+		assertTrue(calc.esbisiesto(2024),
+				"El numero 2024 deberia devolver true");
+			assertFalse(calc.esbisiesto(2023),
+				"El numero 2023 deberia devolver false");	
+			assertTrue(calc.esbisiesto(2000),
+					"El numero 2020 deberia devolver true");
+				assertFalse(calc.esbisiesto(2005),
+					"El numero 2022 deberia devolver false");	
+
+		
+	}
+	@Test
+	void esbisiesto2() {
+	if ((anio % 4 == 0) ;
+		System.out.println("true");
+	else
+		System.out.println("false");
+}
+
+}
